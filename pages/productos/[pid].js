059dd5cba3fcdc1958435f550896bf93f7d0533f
Yosiet <yosietserga@gmail.com>
2021-10-04 19:25:13 -0400

    updates

--- pages/productos/[pid].js
+++ pages/productos/[pid].js
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import Container from "../../components/layout/container";
 import Calculator from "../../components/calculator";
+import Link from "next/link";
+
+const message = "Deseo solicitar infomación sobre GanaRendimiento";
 
 const products = {
   "gana-rendimiento": {
@@ -11,11 +14,119 @@ const products = {
   },
 };
 
+const ContentGanaInversiones = (
+  <div className="col-md-6 col-sm-6  col-xs-12">
+    <div className="table-list">
+      <div className="top-price-inner">
+        <div className="price-title">
+          <h4 className="text-green">GanaInversiones - FIA</h4>
+        </div>
+        <div className="rates">
+          Moneda: <span className="users">Dolares</span>
+        </div>
+      </div>
+      <ol>
+        <li className="">
+          <strong>
+            Inversiones a Corto Plazo concentradas en USD, perfil conservador
+          </strong>
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Monto de apertura:</strong> USD
+          100
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Rescates permitidos:</strong> Sin
+          Restricciones. Sin previa notificación $us. 100.000
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Política de rescate:</strong> Con
+          3 dias hábiles de notificación de $us. 100.001 a $us.500.000 <br />
+          Con 4 días hábiles mayores a $us. 500.000
+        </li>
+        <li className="check">
+          <strong className="text-light-green">
+            Permanencia mínima de la Inversión en cuotas:
+          </strong>{" "}
+          1 día
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Perfil Inversor:</strong>{" "}
+          Invesores conservadores que desean un rendimiento atractivo en USD, y
+          alta disponibilidad de sus recursos
+        </li>
+      </ol>
+      <div className="price-btn">
+        <Link
+          href={{
+            pathname: "contacto/[message]",
+            query: {
+              message,
+            },
+          }}
+          as={`contacto?m=${encodeURIComponent(message)}`}
+        >
+          <a>Solicitar</a>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+
+const ContentGanaRendimiento = (
+  <div className="col-md-7 col-sm-6 col-xs-12">
+    <div className="table-list">
+      <div className="top-price-inner">
+        <div className="price-title dark">
+          <h4 className="">GanaRendimiento - FIA</h4>
+        </div>
+        <div className="rates">
+          Moneda: <span className="users">Bolivianos</span>
+        </div>
+      </div>
+      <ol>
+        <li className="">
+          <strong>
+            Inversiones a Corto Plazo concentradas en Bs y también en USD,
+            perfil conservador
+          </strong>
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Monto de apertura:</strong> Bs.
+          1000
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Rescates permitidos:</strong> Sin
+          Restricciones. sin previa notificación Bs. 700.000
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Política de rescate:</strong> Con
+          3 dias hábiles de notificación de bs. 700.001 a Bs. 3.500.000 <br />
+          Con 4 días hábiles mayores a Bs.3.500.001
+        </li>
+        <li className="check">
+          <strong className="text-light-green">
+            Permanencia mínima de la Inversión en cuotas:
+          </strong>{" "}
+          1 día
+        </li>
+        <li className="check">
+          <strong className="text-light-green">Perfil Inversor:</strong>{" "}
+          Invesores conservadores que desean un rendimiento atractivo en Bs, y
+          alta disponibilidad de sus recursos
+        </li>
+      </ol>
+      <div className="price-btn">
+        <a href="">Solicitar</a>
+      </div>
+    </div>
+  </div>
+);
+
 export default function Producto({ data }) {
   const router = useRouter();
   const { pid } = router.query;
 
-  console.log({data});
   return (
     <Container>
       <div className="pricing-area area-padding-3">
@@ -29,8 +140,13 @@ export default function Producto({ data }) {
                   </h3>
 
                   <p>
-                   Ganadero Sociedad Administradora de Fondos de Inversión S.A. GanaSafi S.A. 
-Es miembro del grupo Financiero Ganadero, nace el año 2020, con la misión de Administrar Fondos de Inversión que coadyuven activamente en el desarrollo económico, financiero y social del país, fomentando el ahorro interno nacional, facilitando económicamente la creación, expansión y desarrollo de empresas nacionales
+                    Ganadero Sociedad Administradora de Fondos de Inversión S.A.
+                    GanaSafi S.A. Es miembro del grupo Financiero Ganadero, nace
+                    el año 2020, con la misión de Administrar Fondos de
+                    Inversión que coadyuven activamente en el desarrollo
+                    económico, financiero y social del país, fomentando el
+                    ahorro interno nacional, facilitando económicamente la
+                    creación, expansión y desarrollo de empresas nacionales
                   </p>
                   <hr />
                   <p>
@@ -49,6 +165,7 @@ Es miembro del grupo Financiero Ganadero, nace el año 2020, con la misión de A
                   </a>
                 </div>
               </div>
+<<<<<<< Updated upstream
 
               <div className="col-md-7 col-sm-6 col-xs-12">
                 <div className="table-list">
@@ -107,6 +224,10 @@ Es miembro del grupo Financiero Ganadero, nace el año 2020, con la misión de A
                   </div>
                 </div>
               </div>
+=======
+              {pid == "gana-rendimiento" && ContentGanaRendimiento}
+              {pid == "gana-inversiones" && ContentGanaInversiones}
+>>>>>>> Stashed changes
             </div>
           </div>
         </div>
@@ -133,12 +254,14 @@ Es miembro del grupo Financiero Ganadero, nace el año 2020, con la misión de A
 export async function getServerSideProps({ params }) {
   const { pid } = params;
   let data = {};
-  let r = await fetch(process.env.BASE_URL + '/api/products/?where={"slug":"'+ pid +'"}');
+  let r = await fetch(
+    process.env.BASE_URL + '/api/products/?where={"slug":"' + pid + '"}'
+  );
 
-    if (r.status < 300) {
-      let d = await r.json();
-       data = d.length>0 ? d[0] : {};
-    }
+  if (r.status < 300) {
+    let d = await r.json();
+    data = d.length > 0 ? d[0] : {};
+  }
 
   //simulating product not found after db request
   if (typeof products[pid] == "undefined") {
