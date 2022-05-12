fc2f978f02418d08fadd0449e59ed24a111b9121
Yosiet <yosietserga@gmail.com>
2022-05-12 15:18:21 -0400

    Obfuscate main security files

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,160 +1,262 @@
-import * as common from "../utils/common.js";
+function _0x5568(_0x13995b, _0x415784) {
+  const _0x589a26 = _0x589a();
+  return (
+    (_0x5568 = function (_0x556817, _0x2d4e1d) {
+      _0x556817 = _0x556817 - 0x10f;
+      let _0x2e6521 = _0x589a26[_0x556817];
+      return _0x2e6521;
+    }),
+    _0x5568(_0x13995b, _0x415784)
+  );
+}
+const _0x1d855b = _0x5568;
+(function (_0x1d9280, _0x3e5a1d) {
+  const _0x37960f = _0x5568,
+    _0x263430 = _0x1d9280();
+  while (!![]) {
+    try {
+      const _0x2cb284 =
+        (parseInt(_0x37960f(0x128)) / 0x1) *
+          (parseInt(_0x37960f(0x133)) / 0x2) +
+        (-parseInt(_0x37960f(0x140)) / 0x3) *
+          (parseInt(_0x37960f(0x149)) / 0x4) +
+        -parseInt(_0x37960f(0x137)) / 0x5 +
+        -parseInt(_0x37960f(0x13c)) / 0x6 +
+        -parseInt(_0x37960f(0x11b)) / 0x7 +
+        (-parseInt(_0x37960f(0x114)) / 0x8) *
+          (parseInt(_0x37960f(0x136)) / 0x9) +
+        parseInt(_0x37960f(0x139)) / 0xa;
+      if (_0x2cb284 === _0x3e5a1d) break;
+      else _0x263430["push"](_0x263430["shift"]());
+    } catch (_0x4e08c4) {
+      _0x263430["push"](_0x263430["shift"]());
+    }
+  }
+})(_0x589a, 0xa9dc7);
+import * as _0x3773c8 from "../utils/common.js";
 import { NextResponse } from "next/server";
-
 let __cookies = {};
-const __cookie_nonce = common.hash("nonces");
-const port = process.env.PORT ?? 3000;
-const base_url = process.env.BASE_URL + ":" + port;
-const script_sources = process.env.CSP_SCRIPT_SOURCE?.split(",").map((s) =>
-  s.trim()
-);
-const style_sources = process.env.CSP_STYLE_SOURCE?.split(",").map((s) =>
-  s.trim()
-);
-const font_sources = process.env.CSP_FONT_SOURCE?.split(",").map((s) =>
-  s.trim()
-);
-const default_sources = process.env.CSP_DEFAULT_SOURCE?.split(",").map((s) =>
-  s.trim()
-);
-
-function csp(req, res) {
-    //const isProduction = process.env.NODE_ENV == "production";
-    const isProduction = true; //forcing env only for testing
-    const devScriptPolicy = ["unsafe-eval", "unsafe-inline"]; // NextJS uses react-refresh in dev
-    const devStylePolicy = ["unsafe-inline"]; // NextJS uses react-refresh in dev
-
-    const styleNonce = common.nonceGenerator();
-    const scriptsNonce = common.nonceGenerator();
-    const defaultNonce = common.nonceGenerator();
-    const fontNonce = common.nonceGenerator();
-
-    setCookie("styleNonce", styleNonce);
-    setCookie("scriptsNonce", scriptsNonce);
-    setCookie("defaultNonce", defaultNonce);
-    setCookie("fontNonce", fontNonce);
-    
-    const cookies = common.encryptObject(__cookies);
-    res.cookie(__cookie_nonce, cookies);
-
-    
-
-    res.headers.append(
-      "Content-Security-Policy",
+const __cookie_nonce = _0x3773c8[_0x1d855b(0x14b)](_0x1d855b(0x115)),
+  port = process[_0x1d855b(0x11d)]["PORT"] ?? 0xbb8,
+  base_url = process[_0x1d855b(0x11d)]["BASE_URL"] + ":" + port,
+  script_sources = process[_0x1d855b(0x11d)][_0x1d855b(0x122)]
+    ?.["split"](",")
+    ["map"]((_0x3664db) => _0x3664db["trim"]()),
+  style_sources = process[_0x1d855b(0x11d)][_0x1d855b(0x118)]
+    ?.[_0x1d855b(0x113)](",")
+    [_0x1d855b(0x147)]((_0x551e21) => _0x551e21[_0x1d855b(0x112)]()),
+  font_sources = process[_0x1d855b(0x11d)][_0x1d855b(0x11c)]
+    ?.[_0x1d855b(0x113)](",")
+    [_0x1d855b(0x147)]((_0x18328b) => _0x18328b[_0x1d855b(0x112)]()),
+  default_sources = process[_0x1d855b(0x11d)][_0x1d855b(0x143)]
+    ?.[_0x1d855b(0x113)](",")
+    [_0x1d855b(0x147)]((_0x6b4d9d) => _0x6b4d9d[_0x1d855b(0x112)]());
+function csp(_0x23bd19, _0x5bfcb2) {
+  const _0x3db28b = _0x1d855b,
+    _0x2b1dfb = !![],
+    _0x240ac3 = [_0x3db28b(0x12a), _0x3db28b(0x11f)],
+    _0x1655c9 = [_0x3db28b(0x11f)],
+    _0x407efa = _0x3773c8[_0x3db28b(0x127)](),
+    _0x27d74f = _0x3773c8[_0x3db28b(0x127)](),
+    _0x277524 = _0x3773c8[_0x3db28b(0x127)](),
+    _0x5d3a1b = _0x3773c8[_0x3db28b(0x127)]();
+  setCookie(_0x3db28b(0x13a), _0x407efa),
+    setCookie(_0x3db28b(0x125), _0x27d74f),
+    setCookie("defaultNonce", _0x277524),
+    setCookie("fontNonce", _0x5d3a1b);
+  const _0x66bd24 = _0x3773c8["encryptObject"](__cookies);
+  _0x5bfcb2["cookie"](__cookie_nonce, _0x66bd24),
+    _0x5bfcb2["headers"]["append"](
+      _0x3db28b(0x142),
       [
-        ["default-src", "self", "nonce-" + defaultNonce].concat(
-          default_sources ?? []
-        ),
-        ["script-src", "self"]
-          .concat(script_sources ?? [])
-          //.concat(isProduction ? ["nonce-" + scriptsNonce] : devScriptPolicy),
-          .concat(isProduction ? devScriptPolicy : devScriptPolicy),
-        ["connect-src", "self", "nonce-" + defaultNonce],
-        ["img-src", "self", "nonce-" + defaultNonce],
-        ["font-src", "self", "nonce-" + fontNonce].concat(font_sources ?? []),
-        ["style-src", "self"]
-          .concat(style_sources ?? [])
-          .concat(
-            isProduction
-              ? ["nonce-" + styleNonce]
-              : devStylePolicy
-          ),
-        ["base-uri", "self", "nonce-" + defaultNonce],
-        ["form-action", "self", "nonce-" + defaultNonce],
-        ["report-to", "main-endpoint"],
-      ].reduce((prev, [directive, ...policy]) => {
-        return `${prev}${directive} ${policy
-          .filter(Boolean)
-          .map((src) => (src.startsWith("https") ? src : `'${src}'`))
-          .join(" ")};`;
+        ["default-src", _0x3db28b(0x121), _0x3db28b(0x12b) + _0x277524][
+          _0x3db28b(0x124)
+        ](default_sources ?? []),
+        ["script-src", _0x3db28b(0x121)]
+          [_0x3db28b(0x124)](script_sources ?? [])
+          [_0x3db28b(0x124)](_0x2b1dfb ? _0x240ac3 : _0x240ac3),
+        ["connect-src", _0x3db28b(0x121), _0x3db28b(0x12b) + _0x277524],
+        [_0x3db28b(0x146), _0x3db28b(0x121), "nonce-" + _0x277524],
+        [_0x3db28b(0x12d), _0x3db28b(0x121), _0x3db28b(0x12b) + _0x5d3a1b][
+          "concat"
+        ](font_sources ?? []),
+        [_0x3db28b(0x13d), "self"]
+          [_0x3db28b(0x124)](style_sources ?? [])
+          ["concat"](_0x2b1dfb ? ["nonce-" + _0x407efa] : _0x1655c9),
+        [_0x3db28b(0x138), _0x3db28b(0x121), "nonce-" + _0x277524],
+        ["form-action", _0x3db28b(0x121), _0x3db28b(0x12b) + _0x277524],
+        ["report-to", _0x3db28b(0x14a)],
+      ][_0x3db28b(0x110)]((_0x265b40, [_0x5c0177, ..._0x49b2dc]) => {
+        const _0x2243ad = _0x3db28b;
+        return (
+          "" +
+          _0x265b40 +
+          _0x5c0177 +
+          "\x20" +
+          _0x49b2dc[_0x2243ad(0x123)](Boolean)
+            [_0x2243ad(0x147)]((_0x4df28c) =>
+              _0x4df28c[_0x2243ad(0x132)]("https")
+                ? _0x4df28c
+                : "\x27" + _0x4df28c + "\x27"
+            )
+            [_0x2243ad(0x141)]("\x20") +
+          ";"
+        );
       }, "")
-    );
-
-    //api csp legacy
-    res.headers.append(
+    ),
+    _0x5bfcb2[_0x3db28b(0x120)][_0x3db28b(0x13e)](
       "Report-To",
-      JSON.stringify([
+      JSON[_0x3db28b(0x148)]([
         {
-          group: "csp-endpoint",
-          max_age: "10886400", //TODO: check and validate expiry date
-          endpoints: [
-            {
-              url: base_url + "/api/security?report=csp",
-            },
-          ],
-          include_subdomains: true,
+          group: _0x3db28b(0x129),
+          max_age: "10886400",
+          endpoints: [{ url: base_url + _0x3db28b(0x10f) }],
+          include_subdomains: !![],
         },
         {
-          group: "network-errors",
-          max_age: "10886400", //TODO: check and validate expiry date
-          endpoints: [
-            {
-              url: base_url + "/api/security?report=network",
-            },
-          ],
-          include_subdomains: true,
-        }
-    ])
-    );
-
-    //api csp legacy
-    res.headers.append(
-      "Report-To",
-      JSON.stringify()
-    );
-
-    //current api reporting csp
-    res.headers.append(
-        "Reporting-Endpoints", 
-        `main-endpoint="${base_url+"/api/security"}", default="${base_url+"/api/security"}"`
+          group: _0x3db28b(0x13f),
+          max_age: _0x3db28b(0x12f),
+          endpoints: [{ url: base_url + _0x3db28b(0x11a) }],
+          include_subdomains: !![],
+        },
+      ])
+    ),
+    _0x5bfcb2[_0x3db28b(0x120)][_0x3db28b(0x13e)](
+      _0x3db28b(0x130),
+      JSON[_0x3db28b(0x148)]()
+    ),
+    _0x5bfcb2[_0x3db28b(0x120)]["append"](
+      _0x3db28b(0x117),
+      _0x3db28b(0x135) +
+        (base_url + _0x3db28b(0x111)) +
+        _0x3db28b(0x145) +
+        (base_url + _0x3db28b(0x111)) +
+        "\x22"
     );
 }
-
-function loadCookies(str) {
-    if (typeof str !== "string" || !str) return false;
-    try {
-        const o = common.decryptObject( str );
-        __cookies = typeof o === "object" ? common.decryptObject( str ) : {};
-    } catch (error) {
-        common.log( error );
-        __cookies = {};
-    }
-}
-
-function setCookie(k, v) {
+function loadCookies(_0x33eee0) {
+  const _0x53f429 = _0x1d855b;
+  if (typeof _0x33eee0 !== _0x53f429(0x119) || !_0x33eee0) return ![];
   try {
-    const __key = common.hashCookies ? common.hash(k) : k;
-    __cookies[__key] = v;
-  } catch (error) {
-    console.error({ error });
+    const _0x1e578e = _0x3773c8[_0x53f429(0x131)](_0x33eee0);
+    __cookies =
+      typeof _0x1e578e === _0x53f429(0x12e)
+        ? _0x3773c8[_0x53f429(0x131)](_0x33eee0)
+        : {};
+  } catch (_0xaf3aa) {
+    _0x3773c8["log"](_0xaf3aa), (__cookies = {});
   }
 }
-
-export function getCookie(k = null) {
+function setCookie(_0x17e80e, _0x351d06) {
+  const _0x219276 = _0x1d855b;
   try {
-    const __key = common.hashCookies ? common.hash(k) : k;
-    return !k ? __cookies : !!__cookies[__key] ? __cookies[__key] : null;
-  } catch (error) {
-    console.error({ error });
+    const _0x2c4151 = _0x3773c8[_0x219276(0x116)]
+      ? _0x3773c8[_0x219276(0x14b)](_0x17e80e)
+      : _0x17e80e;
+    __cookies[_0x2c4151] = _0x351d06;
+  } catch (_0x19e5c0) {
+    console[_0x219276(0x134)]({ error: _0x19e5c0 });
   }
 }
-
-export function writeCookies(res) {
+function _0x589a() {
+  const _0x321cfb = [
+    "4694210gQmrmd",
+    "base-uri",
+    "30099360YffXjT",
+    "styleNonce",
+    "name",
+    "4687152MhFVAz",
+    "style-src",
+    "append",
+    "network-errors",
+    "6HVQYtm",
+    "join",
+    "Content-Security-Policy",
+    "CSP_DEFAULT_SOURCE",
+    "page",
+    "\x22,\x20default=\x22",
+    "img-src",
+    "map",
+    "stringify",
+    "1283960iMAmCm",
+    "main-endpoint",
+    "hash",
+    "/api/security?report=csp",
+    "reduce",
+    "/api/security",
+    "trim",
+    "split",
+    "366888oJXowe",
+    "nonces",
+    "hashCookies",
+    "Reporting-Endpoints",
+    "CSP_STYLE_SOURCE",
+    "string",
+    "/api/security?report=network",
+    "8208123bOSetd",
+    "CSP_FONT_SOURCE",
+    "env",
+    "cookie",
+    "unsafe-inline",
+    "headers",
+    "self",
+    "CSP_SCRIPT_SOURCE",
+    "filter",
+    "concat",
+    "scriptsNonce",
+    "undefined",
+    "nonceGenerator",
+    "1358001vLFDfQ",
+    "csp-endpoint",
+    "unsafe-eval",
+    "nonce-",
+    "COOKIE_PATH",
+    "font-src",
+    "object",
+    "10886400",
+    "Report-To",
+    "decryptObject",
+    "startsWith",
+    "2WoXUyn",
+    "error",
+    "main-endpoint=\x22",
+    "27lcOdst",
+  ];
+  _0x589a = function () {
+    return _0x321cfb;
+  };
+  return _0x589a();
+}
+export function getCookie(_0x589b4e = null) {
+  const _0x523579 = _0x1d855b;
   try {
-    res.cookie(common.COOKIE_PATH, cookies);
-  } catch (error) {
-    console.error({ error });
+    const _0x4a03e4 = _0x3773c8[_0x523579(0x116)]
+      ? _0x3773c8[_0x523579(0x14b)](_0x589b4e)
+      : _0x589b4e;
+    return !_0x589b4e
+      ? __cookies
+      : !!__cookies[_0x4a03e4]
+      ? __cookies[_0x4a03e4]
+      : null;
+  } catch (_0x5762e2) {
+    console[_0x523579(0x134)]({ error: _0x5762e2 });
   }
 }
-
-export const middleware = (req, event) => {
-    const res = NextResponse.next();
-  
-    //load cookies from request
-    if (typeof req?.page?.name !== "undefined") loadCookies(req.cookies[__cookie_nonce]);
-
-    //write CSP
-    csp(req, res);
-    
-    return res;
+export function writeCookies(_0x3f6258) {
+  const _0x1274c1 = _0x1d855b;
+  try {
+    _0x3f6258[_0x1274c1(0x11e)](_0x3773c8[_0x1274c1(0x12c)], cookies);
+  } catch (_0x23566a) {
+    console[_0x1274c1(0x134)]({ error: _0x23566a });
+  }
+}
+export const middleware = (_0x334839, _0x3a5807) => {
+  const _0x4f6b13 = _0x1d855b,
+    _0x544e93 = NextResponse["next"]();
+  if (
+    typeof _0x334839?.[_0x4f6b13(0x144)]?.[_0x4f6b13(0x13b)] !==
+    _0x4f6b13(0x126)
+  )
+    loadCookies(_0x334839["cookies"][__cookie_nonce]);
+  return csp(_0x334839, _0x544e93), _0x544e93;
 };
